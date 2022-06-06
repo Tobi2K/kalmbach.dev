2b7e4537f4aa5a4e1bf27c2e8669c82508a334cf
Tobias Kalmbach <t.kalmbach35@gmail.com>
2022-06-06 20:29:25 +0200

    Add Header

---
+++ components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0"
+    
+  >
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="mt-8 text-white overflow-hidden p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Welcome to my Portfolio
+        </h2>
+        <p class="mt-3">
+          My name is Tobi and I have been coding since 2016. I hope to inspire you with some of my following projects.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderJumbotron',
+}
+</script>

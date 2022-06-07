608181eea8b29a9711ac5787fec8076963942a57
Tobias Kalmbach <t.kalmbach35@gmail.com>
2022-06-07 19:32:40 +0200

    Add Title animation WIP

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,32 @@
 <template>
   <div
     class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0"
-    
   >
     <link
       href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
       rel="stylesheet"
     />
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+    <div class="container max-w-4xl mx-auto sm:px-6 lg:px-8">
       <div class="mt-8 text-white overflow-hidden p-6">
+        <header class="mb-16 group">
+          <h1 class="mb-1 font-mono text-4xl text-gray-100 md:text-6xl">
+            Hi,<br class="block md:hidden" />
+            <span
+              class="inline-flex h-20 -ml-6 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform"
+            >
+              I'm Tobi 👋
+            </span>
+            <span
+              class="box-border inline-block w-1 h-10 -ml-6 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"
+            ></span>
+          </h1>
+        </header>
         <h2 class="text-2xl leading-7 font-semibold">
-          Welcome to my Portfolio
+          This is my personal website
         </h2>
         <p class="mt-3">
-          My name is Tobi and I have been coding since 2016. I hope to inspire you with some of my following projects.
+          I have been coding since 2016. I hope to inspire you with some of my
+          projects.
         </p>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -14,4 +14,31 @@ export default Vue.extend({
 body {
   background-image: url(./../static/bg.svg);
 }
+
+.will-change-transform {
+  will-change: transform;
+}
+
+.cursor::after {
+  display: block;
+  content: '';
+  position: absolute;
+  width: 4px;
+  height: 100%;
+  background-color: #fff;
+  animation: cursor 0.6s linear infinite alternate;
+  will-change: opacity;
+}
+
+@keyframes cursor {
+  0%,
+  40% {
+    opacity: 1;
+  }
+
+  60%,
+  100% {
+    opacity: 0;
+  }
+}
 </style>

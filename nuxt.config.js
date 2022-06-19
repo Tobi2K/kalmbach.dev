d606009c24f588ceddb4eb34d5d8ba2b8b2f8be3
Tobias Kalmbach <t.kalmbach35@gmail.com>
2022-06-19 18:26:41 +0200

    Edit meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'kalmbach.dev',
+    title: 'Tobias Kalmbach',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

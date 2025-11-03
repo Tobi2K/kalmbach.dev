797b3879e6d2d583e5ea418130843770356008e3
Tobias Kalmbach <tobias@kalmbach.dev>
2025-11-03 15:24:25 +0100

    fix: Correct port

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,59 +2,63 @@ export default defineNuxtConfig({
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  server: {
+  devServer: {
     port: 8080,
   },
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Portfolio & more | Tobias Kalmbach | Software Engineer',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content:
-          "I'm Tobi, a software developer and student. Feel free to check out my projects or connect with me!",
-      },
-      { name: 'author', content: 'Tobias Kalmbach' },
-      { name: 'application-name', content: 'Tobias Kalmbach' },
-      { name: 'google-adsense-account', content: 'ca-pub-1622848203679095' },
-      { hid: 'og-type', property: 'og:type', content: 'website' },
-      {
-        hid: 'og-title',
-        property: 'og:title',
-        content: 'Portfolio & more | Tobias Kalmbach | Software Engineer',
-      },
-      {
-        hid: 'og-description',
-        property: 'og:description',
-        content:
-          "I'm Tobi, a software developer and student. Feel free to check out my projects or connect with me!",
-      },
-      { hid: 'og-url', property: 'og:url', content: 'https://kalmbach.dev/' },
+  app: {
+      // Global page headers: https://go.nuxtjs.dev/config-head
+      head: {
+        title: 'Portfolio & more | Tobias Kalmbach | Software Engineer',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          {
+            hid: 'description',
+            name: 'description',
+            content:
+              "I'm Tobi, a software developer and student. Feel free to check out my projects or connect with me!",
+          },
+          { name: 'author', content: 'Tobias Kalmbach' },
+          { name: 'application-name', content: 'Tobias Kalmbach' },
+          { name: 'google-adsense-account', content: 'ca-pub-1622848203679095' },
+          { hid: 'og-type', property: 'og:type', content: 'website' },
+          {
+            hid: 'og-title',
+            property: 'og:title',
+            content: 'Portfolio & more | Tobias Kalmbach | Software Engineer',
+          },
+          {
+            hid: 'og-description',
+            property: 'og:description',
+            content:
+              "I'm Tobi, a software developer and student. Feel free to check out my projects or connect with me!",
+          },
+          { hid: 'og-url', property: 'og:url', content: 'https://kalmbach.dev/' },
 
-      {
-        hid: 'twitter-title',
-        property: 'twitter:title',
-        content: 'Portfolio & more | Tobias Kalmbach | Software Engineer',
+          {
+            hid: 'twitter-title',
+            property: 'twitter:title',
+            content: 'Portfolio & more | Tobias Kalmbach | Software Engineer',
+          },
+          {
+            hid: 'twitter-description',
+            property: 'twitter:description',
+            content:
+              "I'm Tobi, a software developer and student. Feel free to check out my projects or connect with me!",
+          },
+        ],
+        link: [
+          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+          {
+            rel: 'canonical',
+            href: 'https://kalmbach.dev/',
+          },
+        ],
       },
-      {
-        hid: 'twitter-description',
-        property: 'twitter:description',
-        content:
-          "I'm Tobi, a software developer and student. Feel free to check out my projects or connect with me!",
-      },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'canonical',
-        href: 'https://kalmbach.dev/',
-      },
-    ],
   },
+
+
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/css/style.css'],
